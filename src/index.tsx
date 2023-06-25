40a5b3b999fba1ed82ae86cb300e3bdf1846bef5
Angel Fumero Revetti <angel.fumero.revetti@gmail.com>
2023-06-25 19:08:15 +0100

    Add missing import

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {MantineProvider} from "@mantine/core";
 
+import "reflect-metadata";
+
 import App from "./app/App";
 import {ErrorPage, HomePage} from "./app/pages";
 
